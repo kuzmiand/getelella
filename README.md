d40ec67e796954b2ffc245cda005a095573e8083
kuzmaind <rabota.kuzmiand@gmail.com>
2016-07-19 10:25:23 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+Installation
+------------
+
+Either run
+
+```
+php composer.phar require --prefer-dist yiister/yii2-gentelella "*"
+```
+
+or add
+
+```
+"yiister/yii2-gentelella": "*"
+```
+
+to the require section of your `composer.json` file.
---
+++ assets/ThemeAsset.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace yiister\gentelella\assets;
+
+use yii\web\AssetBundle;
+
+class ThemeAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/bower/gentelella/build/';
+    public $css = [
+        'css/custom.css',
+    ];
+    public $js = [
+        'js/custom.js',
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapPluginAsset',
+        'rmrevin\yii\fontawesome\AssetBundle',
+    ];
+}
